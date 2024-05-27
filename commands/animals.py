6bf61f83840f1ed824741571ec71e83c1fb1639d
Restart <79792966+RestartB@users.noreply.github.com>
2024-05-27 21:49:23 +0100

    feat: :sparkles: Add sand cat command

--- commands/animals.py
+++ commands/animals.py
@@ -3,10 +3,13 @@ from discord import app_commands, Color
 from discord.ext import commands
 import random
 import aiohttp
+import os
+from pathlib import Path
 
 class animals(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.sandcat_files = os.listdir(f"{self.bot.path}{self.bot.pathtype}content{self.bot.pathtype}sand-cat{self.bot.pathtype}")
 
     global cat_titles, dog_titles
     
@@ -43,6 +46,25 @@ class animals(commands.Cog):
                 embed.set_image(url = request_data["message"])
                 embed.set_footer(text = f"Requested by {interaction.user.name}", icon_url = interaction.user.avatar.url)
                 await interaction.followup.send(embed = embed)
+    
+    # Sand Cat command
+    @animalGroup.command(name = "sand-cat", description = "Get a random sand cat picture.")
+    @app_commands.checks.cooldown(1, 5)
+    async def sand_cat(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        
+        try:
+            target_file = random.choice(self.sandcat_files)
+            
+            # Make embed, send it
+            embed = discord.Embed(title = random.choice(cat_titles), description = "Image credit: https://display-a.sand.cat/", color = Color.random())
+            file = discord.File(f"{self.bot.path}{self.bot.pathtype}content{self.bot.pathtype}sand-cat{self.bot.pathtype}{target_file}", filename = target_file)
+            embed.set_image(url = f"attachment://{target_file}")
+            embed.set_footer(text = f"Requested by {interaction.user.name}", icon_url = interaction.user.avatar.url)
+            await interaction.followup.send(file=file, embed = embed)
+        except Exception:
+            embed = discord.Embed(title = "Unexpected Error", description = "Please try again later or message <@563372552643149825> for assistance.", color = Color.red())
+            await interaction.edit_original_response(embed = embed, view = None)
 
 async def setup(bot):
     await bot.add_cog(animals(bot))
