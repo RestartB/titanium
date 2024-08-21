2529ec82a99073518481a893e9d3c580a161d4d9
Restart <79792966+RestartB@users.noreply.github.com>
2024-08-21 18:09:44 +0100

    fix: :bug: Fix status updater not looping

--- commands/status_update.py
+++ commands/status_update.py
@@ -9,14 +9,15 @@ class status_update(commands.Cog):
     # Listen for Messages
     @commands.Cog.listener()
     async def on_ready(self):
-        members = 0
-        
-        for guild in self.bot.guilds:
-            members += guild.member_count
+        while True:
+            members = 0
+            
+            for guild in self.bot.guilds:
+                members += guild.member_count
 
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{members} users in {len(self.bot.guilds)} servers - / to see commands"))
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{members} users in {len(self.bot.guilds)} servers - / to see commands"))
 
-        await asyncio.sleep(86400)
+            await asyncio.sleep(86400)
 
 async def setup(bot):
     await bot.add_cog(status_update(bot))
