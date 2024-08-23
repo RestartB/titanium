fbb3b77a07f0b00f56d4c81ab9589c4ccfbb9d5e
Restart <79792966+RestartB@users.noreply.github.com>
2024-08-23 21:45:53 +0100

    style: :pencil2: Adjust status update

--- commands/status_update.py
+++ commands/status_update.py
@@ -6,18 +6,21 @@ class status_update(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    # Listen for Messages
+    # Status Update
     @commands.Cog.listener()
     async def on_ready(self):
         while True:
+            # Count members
             members = 0
             
             for guild in self.bot.guilds:
                 members += guild.member_count
 
+            # Update status
             await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{members} users in {len(self.bot.guilds)} servers - / to see commands"))
 
-            await asyncio.sleep(86400)
+            # Wait 1 hour
+            await asyncio.sleep(3600)
 
 async def setup(bot):
     await bot.add_cog(status_update(bot))
