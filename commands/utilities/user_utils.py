1d0925dacf62e73c0d7bf9769a4cb535587ac482
RestartB <79792966+RestartB@users.noreply.github.com>
2024-12-11 22:01:45 +0000

    feat: Add hat size selection to christmas PFP

--- commands/utilities/user_utils.py
+++ commands/utilities/user_utils.py
@@ -55,9 +55,12 @@ class UserUtils(commands.Cog):
                 roles.append(role.mention)
             
             embed.add_field(name = "Roles", value = ", ".join(roles))
+
+            embed.set_footer(text = f"@{interaction.user.name}", icon_url = interaction.user.display_avatar.url)
+        else:
+            embed.set_footer(text = f"@{interaction.user.name} - Want more info? Add the bot to the server!", icon_url = interaction.user.display_avatar.url)
         
         embed.set_thumbnail(url = member.display_avatar.url)
-        embed.set_footer(text = f"@{interaction.user.name}", icon_url = interaction.user.display_avatar.url)
         
         view = View()
         view.add_item(discord.ui.Button(label="User URL", style=discord.ButtonStyle.url, url=f"https://discord.com/users/{user.id}", row = 0))
@@ -93,8 +96,14 @@ class UserUtils(commands.Cog):
     @app_commands.describe(user = "The target user.")
     @app_commands.describe(hat = "Optional: whether to add a christmas hat. Defaults to true.")
     @app_commands.describe(snow = "Optional: whether to add snow. Defaults to true.")
+    @app_commands.describe(position = "Optional: the size of the hat on the user's head when enabled. Defaults to normal.")
     @app_commands.describe(position = "Optional: the position of the hat on the user's head when enabled. Defaults to top middle.")
     @app_commands.describe(ephemeral = "Optional: whether to send the command output as a dismissible message only visible to you. Defaults to false.")
+    @app_commands.choices(hat_size=[
+            app_commands.Choice(name="Small", value=6),
+            app_commands.Choice(name="Normal", value=4),
+            app_commands.Choice(name="Large", value=2),
+            ])
     @app_commands.choices(position=[
             app_commands.Choice(name="Top Left", value="topleft"),
             app_commands.Choice(name="Top Middle", value="topmiddle"),
@@ -103,12 +112,15 @@ class UserUtils(commands.Cog):
             app_commands.Choice(name="Bottom Middle", value="bottommiddle"),
             app_commands.Choice(name="Bottom Right", value="bottomright"),
             ])
-    async def christmas(self, interaction: discord.Interaction, user: discord.User, hat: bool = True, snow: bool = True, position: app_commands.Choice[str] = None, ephemeral: bool = False):
+    async def christmas(self, interaction: discord.Interaction, user: discord.User, hat: bool = True, snow: bool = True, size: app_commands.Choice[str] = None, position: app_commands.Choice[str] = None, ephemeral: bool = False):
         await interaction.response.defer(ephemeral=ephemeral)
         
         try:
             if user is None:
                 user = interaction.user
+            
+            if size is None:
+                size = app_commands.Choice(name="Normal", value=4)
             
             if position is None:
                 position = app_commands.Choice(name="Top Middle", value="topmiddle")
@@ -133,7 +145,7 @@ class UserUtils(commands.Cog):
                 hatImg = Image.open(os.path.join("content", "hat.png"))
 
                 # Resize the hat to fit the head - maintain aspect ratio
-                hatImg = hatImg.resize((hatImg.width//4, hatImg.height//4))
+                hatImg = hatImg.resize((hatImg.width//int(size.value), hatImg.height//int(size.value)), Image.Resampling.LANCZOS)
 
                 # Put hat on head in requested position
                 if position.value == "topleft":
