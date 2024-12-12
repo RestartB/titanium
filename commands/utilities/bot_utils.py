ab895c98d4f379f04f12cdbbef838c987075c055
Restart <79792966+RestartB@users.noreply.github.com>
2024-12-12 12:52:07 +0000

    feat: :sparkles: Add bot version checker

--- commands/utilities/bot_utils.py
+++ commands/utilities/bot_utils.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 import cpuinfo
 import discord
 import psutil
+import pygit2
 from discord import Color, app_commands
 from discord.ext import commands
 from discord.ui import View
@@ -53,6 +54,27 @@ class BotUtils(commands.Cog):
         embed = discord.Embed(title = "Titanium", description = "Hi, I'm Titanium! I'm a multi-purpose, open source Discord bot created by Restart (<@563372552643149825>). I use slash commands - use `/` to see all of my commands!", color = Color.green())
         embed.add_field(name = "GitHub", value = "You can also find me on GitHub - this is the place to go if you have found a bug or have a feature suggestion! Just submit an issue and I'll take a look. You can also add a star to show some love to the project. It's free and helps me a lot! https://github.com/restartb/titanium")
         embed.set_thumbnail(url=self.bot.user.display_avatar.url)
+
+        repo = pygit2.Repository('.git')
+        remote = repo.remotes["origin"]
+        remote.fetch()
+
+        # Get local HEAD commit
+        local_head = repo.head.target
+
+        # Get current branch name
+        branch_name = repo.head.shorthand
+
+        # Get remote HEAD for current branch
+        remote_ref = f"refs/remotes/origin/{branch_name}"
+        remote_head = repo.references[remote_ref].target
+
+        # Convert to short hashes
+        local_short = str(local_head)[:7]
+        remote_short = str(remote_head)[:7]
+        synced = local_head == remote_head
+
+        embed.add_field(name = "Current Version", value = (f':white_check_mark: Up to date ({local_short})' if synced else f':x: Out of date ({local_short}, latest: {remote_short})'), inline = False)
         
         view = View()
         view.add_item(discord.ui.Button(label="GitHub", style=discord.ButtonStyle.url, url="https://github.com/restartb/titanium"))
@@ -76,13 +98,12 @@ class BotUtils(commands.Cog):
 
         sysinfo = cpuinfo.get_cpu_info()
 
-        embed.add_field(name = "CPU Name", value = sysinfo['brand_raw'], inline = False)
-        embed.add_field(name = "Percent CPU Usage", value = psutil.cpu_percent(), inline = False)
-        embed.add_field(name = "Percent RAM Usage", value = psutil.virtual_memory().percent, inline = False)
-        embed.add_field(name = "System Uptime", value = ("%d:%d:%d:%d" % (d.day-1, d.hour, d.minute, d.second)), inline = False)
-        embed.add_field(name = "OS Name", value = os.name, inline = False)
         embed.add_field(name = "Python Version", value = sysinfo['python_version'])
-        embed.add_field(name = "Bot Latency", value = f"{round(self.bot.latency*1000, 2)}ms", inline = False)
+        embed.add_field(name = "System Uptime", value = ("%d:%d:%d:%d" % (d.day-1, d.hour, d.minute, d.second)))
+        embed.add_field(name = "Operating System", value = os.name)
+        embed.add_field(name = "CPU Name", value = sysinfo['brand_raw'])
+        embed.add_field(name = "CPU Usage", value = f"{psutil.cpu_percent()}%")
+        embed.add_field(name = "RAM Usage", value = f"{psutil.virtual_memory().percent}%")
         
         embed.set_footer(text = f"@{interaction.user.name}", icon_url = interaction.user.display_avatar.url)
 
