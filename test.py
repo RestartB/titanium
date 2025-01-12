a9feda6a98d1de502824d404c3219591d9490dea
Restart <79792966+RestartB@users.noreply.github.com>
2025-01-12 15:54:51 +0000

    revert: remove test file

--- test.py
+++
@@ -1,6 +0,0 @@
-from thefuzz import process
-
-test = {563372552643149825: [{'name': 'test', 'content': 'test'}, {'name': 'test1', 'content': 'test'}]}
-
-matches = process.extract('test1', [tag['name'] for tag in test[563372552643149825]], limit=10)
-print(matches)
