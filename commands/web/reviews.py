78d30685db1fc46ec134f91413fb9342eba26a3c
Restart <79792966+RestartB@users.noreply.github.com>
2025-02-25 16:31:19 +0000

    fix: add missing else

--- commands/web/reviews.py
+++ commands/web/reviews.py
@@ -73,7 +73,7 @@ class Reviews(commands.Cog):
 
         embed.set_author(
             name=guild.name,
-            icon_url=(guild.icon.url if guild.icon is not None),
+            icon_url=(guild.icon.url if guild.icon is not None else None),
         )
 
         embed.set_footer(
@@ -665,7 +665,7 @@ class Reviews(commands.Cog):
             )
             embed.set_author(
                 name=guild.name,
-                icon_url=(guild.icon.url if guild.icon is not None),
+                icon_url=(guild.icon.url if guild.icon is not None else None),
             )
 
             await interaction.followup.send(embed=embed, ephemeral=ephemeral)
