57dcb26a62e60d38b84ce713b1e06064318dc8df
RestartB <79792966+RestartB@users.noreply.github.com>
2025-04-21 00:29:10 +0100

    fix: fix analytics webhook checks

--- commands/automated/analytics.py
+++ commands/automated/analytics.py
@@ -20,50 +20,18 @@ class Analytics(commands.Cog):
         interaction: discord.Interaction,
         command: Union[app_commands.Command, app_commands.ContextMenu],
     ) -> None:
-        # Ignore if there is no werbhook
-        if (
-            self.bot.options["analytics-webhook"] is not None
-            and self.bot.options["analytics-webhook"] != ""
-        ):
-            try:
-                embed = discord.Embed(
-                    title=f"@{interaction.user.name} ran a command", color=Color.green()
-                )
-                embed.description = f"`/{f'{command.parent.name} ' if command.parent is not None else ''}{command.name}`"
-
-                embed.timestamp = interaction.created_at
-                embed.set_author(
-                    name=str(self.bot.user), icon_url=self.bot.user.display_avatar.url
-                )
-
-                embed.add_field(
-                    name="User",
-                    value=f"{interaction.user.mention} ({interaction.user.id})",
-                )
-
-                async with aiohttp.ClientSession() as session:
-                    webhook = discord.Webhook.from_url(
-                        self.bot.options["analytics-webhook"], session=session
-                    )
-                    await webhook.send(embed=embed)
-            except Exception as e:
-                print(f"[ANALYTICS] Failed to send analytics webhook - {e}")
-
-    # Analytics for raw interactions
-    @commands.Cog.listener()
-    async def on_interaction(self, interaction: discord.Interaction):
-        # Ignore if there is no werbhook
-        if (
-            self.bot.options["raw-analytics-webhook"] is not None
-            and self.bot.options["raw-analytics-webhook"] != ""
-        ):
-            try:
-                async with aiohttp.ClientSession() as session:
+        try:
+            # Ignore if there is no werbhook
+            if (
+                self.bot.options["analytics-webhook"] is not None
+                and self.bot.options["analytics-webhook"] != ""
+            ):
+                try:
                     embed = discord.Embed(
-                        title=f"@{interaction.user.name} started an interaction",
+                        title=f"@{interaction.user.name} ran a command",
                         color=Color.green(),
                     )
-                    embed.description = f"`/{f'{interaction.command.parent.name} ' if interaction.command.parent is not None else ''}{interaction.command.name}`"
+                    embed.description = f"`/{f'{command.parent.name} ' if command.parent is not None else ''}{command.name}`"
 
                     embed.timestamp = interaction.created_at
                     embed.set_author(
@@ -78,23 +46,79 @@ class Analytics(commands.Cog):
 
                     async with aiohttp.ClientSession() as session:
                         webhook = discord.Webhook.from_url(
-                            self.bot.options["raw-analytics-webhook"], session=session
+                            self.bot.options["analytics-webhook"], session=session
                         )
                         await webhook.send(embed=embed)
-            except Exception as e:
-                print(f"[ANALYTICS] Failed to send raw analytics webhook - {e}")
+                except Exception as e:
+                    print(f"[ANALYTICS] Failed to send analytics webhook - {e}")
+        except KeyError:
+            pass
+
+    # Analytics for raw interactions
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction: discord.Interaction):
+        try:
+            # Ignore if there is no werbhook
+            if (
+                self.bot.options["raw-analytics-webhook"] is not None
+                and self.bot.options["raw-analytics-webhook"] != ""
+            ):
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        embed = discord.Embed(
+                            title=f"@{interaction.user.name} started an interaction",
+                            color=Color.green(),
+                        )
+                        embed.description = f"`/{f'{interaction.command.parent.name} ' if interaction.command.parent is not None else ''}{interaction.command.name}`"
+
+                        embed.timestamp = interaction.created_at
+                        embed.set_author(
+                            name=str(self.bot.user),
+                            icon_url=self.bot.user.display_avatar.url,
+                        )
+
+                        embed.add_field(
+                            name="User",
+                            value=f"{interaction.user.mention} ({interaction.user.id})",
+                        )
+
+                        async with aiohttp.ClientSession() as session:
+                            webhook = discord.Webhook.from_url(
+                                self.bot.options["raw-analytics-webhook"],
+                                session=session,
+                            )
+                            await webhook.send(embed=embed)
+                except Exception as e:
+                    print(f"[ANALYTICS] Failed to send raw analytics webhook - {e}")
+        except KeyError:
+            pass
 
 
 async def setup(bot: "TitaniumBot") -> None:
-    # Only load if webhook URL is present
     try:
+        # Only load if webhook URL is present
         if (
             bot.options["analytics-webhook"] is not None
             and bot.options["analytics-webhook"] != ""
-        ) or (
+        ):
+            normal = True
+        else:
+            normal = False
+    except KeyError:
+        normal = False
+
+    try:
+        if (
             bot.options["raw-analytics-webhook"] is not None
             and bot.options["raw-analytics-webhook"] != ""
         ):
-            await bot.add_cog(Analytics(bot))
+            raw = True
+        else:
+            raw = False
     except KeyError:
-        pass
+        raw = False
+
+    if normal or raw:
+        await bot.add_cog(Analytics(bot))
+    else:
+        return
