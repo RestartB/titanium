d02d9e74dba05b811ee51b2d740622596fad3760
RestartB <79792966+RestartB@users.noreply.github.com>
2025-04-21 00:53:46 +0100

    fix: add user install flags

--- commands/misc/quote.py
+++ commands/misc/quote.py
@@ -655,6 +655,8 @@ class Quotes(commands.Cog):
             ),
         ],
     )
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     async def custom_quote(
         self,
         interaction: discord.Interaction,
