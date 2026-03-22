96b361942979bb2fd369075961b3e40304177ae6
RestartB <79792966+RestartB@users.noreply.github.com>
2026-03-22 02:07:49 +0000

    fix: fix migration to uuid

--- migrations/20260322015642.sql
+++ migrations/20260322015642.sql
@@ -1,4 +1,8 @@
 -- Modify "scheduled_tasks" table
-ALTER TABLE "scheduled_tasks" ALTER COLUMN "id" DROP DEFAULT, ALTER COLUMN "id" TYPE uuid;
--- Drop sequence used by serial column "id"
+ALTER TABLE "scheduled_tasks"
+  ALTER COLUMN "id" DROP DEFAULT,
+  ALTER COLUMN "id" TYPE uuid
+  USING lpad(to_hex("id"), 32, '0')::uuid;
+
+-- Drop sequence used by old bigserial
 DROP SEQUENCE IF EXISTS "scheduled_tasks_id_seq";
