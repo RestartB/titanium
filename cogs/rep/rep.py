cca7b316fbefef71a373e6d2de2c128b092c901b
RestartB <79792966+RestartB@users.noreply.github.com>
2026-07-03 22:45:46 +0100

    feat: initial, untested rep commands

--- cogs/rep/rep.py
+++ cogs/rep/rep.py
@@ -2,12 +2,14 @@ import re
 from typing import TYPE_CHECKING
 
 import discord
-from discord import ButtonStyle, Interaction
+from discord import ButtonStyle, Interaction, app_commands
 from discord.ext import commands
 from discord.ui import Button, View, button
+from sqlalchemy import func, select
 from sqlalchemy.dialects.postgresql import insert
 
 from lib.sql.sql import RepAddHistory, UserRep, get_session
+from lib.views.pagination import RepReloadPageView
 
 if TYPE_CHECKING:
     from main import TitaniumBot
@@ -23,13 +25,24 @@ class RepView(View):
         super().__init__(timeout=timeout)
         self.bot = bot
         self.target_member = target_member
+        self.original_message: discord.Message | None
 
     @button(label="Give Rep", emoji="➕", style=ButtonStyle.green)
     async def give_rep(self, interaction: Interaction["TitaniumBot"], button: Button):
-        if not interaction.message or not interaction.guild_id:
+        if not interaction.guild_id:
             return
 
-        await interaction.response.defer()
+        await interaction.response.defer(ephemeral=True)
+
+        guild_config = await self.bot.fetch_guild_config(interaction.guild_id)
+        if not guild_config or not guild_config.rep_enabled:
+            embed = discord.Embed(
+                title=f"{self.bot.error_emoji} Rep Disabled",
+                description="The rep system is disabled in this server. Ask a server admin to turn it on using the `/settings` command or the Titanium Dashboard.",
+                colour=discord.Colour.red(),
+            )
+            await interaction.followup.send(embed=embed, ephemeral=True)
+            return
 
         async with get_session() as session:
             session.add(
@@ -54,20 +67,34 @@ class RepView(View):
 
             rep = (await session.execute(stmt)).scalar_one()
 
-        await interaction.message.edit(
+        await interaction.followup.send(
             embed=discord.Embed(
-                description=f"{self.bot.success_emoji} **1 rep** given to {self.target_member.mention} (`{rep.rep}` rep total)",
+                title=f"{self.bot.success_emoji} Done",
+                description=f"**1 rep** given to {self.target_member.mention} (`{rep.rep}` rep total)",
                 colour=discord.Colour.green(),
             ),
-            view=None,
+            ephemeral=True,
         )
 
     @button(emoji="🗑️", style=ButtonStyle.grey)
     async def delete_button(self, interaction: Interaction["TitaniumBot"], button: Button):
-        if not interaction.message:
+        if not self.original_message:
             return
 
         await interaction.response.defer(ephemeral=True)
+
+        if (
+            self.original_message.author.id != interaction.user.id
+            or not interaction.permissions.manage_messages
+        ):
+            embed = discord.Embed(
+                title=f"{interaction.client.error_emoji} Not Allowed",
+                description="You didn't send this message. Only the message author or users with Manage Message permissions can delete the rep hint.",
+                colour=discord.Colour.red(),
+            )
+            await interaction.followup.send(embed=embed, ephemeral=True)
+            return
+
         await interaction.delete_original_response()
 
 
@@ -77,13 +104,14 @@ class RepTestCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        # no reply / invalid reference - ignore
+        # no reply / invalid reference / opted out - ignore
         if (
             not message.guild
             or not message.reference
             or not message.reference.resolved
             or isinstance(message.reference.resolved, discord.DeletedReferencedMessage)
             or message.reference.type != discord.MessageReferenceType.reply
+            or message.author.id in self.bot.opt_out
         ):
             return
 
@@ -110,7 +138,304 @@ class RepTestCog(commands.Cog):
         view = RepView(bot=self.bot, target_member=message.reference.resolved.author)
         await message.reply(view=view, mention_author=False, delete_after=60)
 
+    @commands.hybrid_group(
+        name="rep", fallback="view", description="Set, add, remove, and view rep for users."
+    )
+    @commands.guild_only()
+    @app_commands.guild_install()
+    @commands.cooldown(1, 3)
+    async def rep_group(self, ctx: commands.Context["TitaniumBot"], member: discord.Member) -> None:
+        if not ctx.guild:
+            raise ValueError("Guild only command but no guild available")
+
+        await ctx.defer()
+        user = member or ctx.author
+
+        if user.id in self.bot.opt_out:
+            embed = discord.Embed(
+                title=f"{self.bot.error_emoji} Opted Out",
+                description="This user has opted out of optional data collection and cannot use rep features.",
+                colour=discord.Colour.red(),
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        async with get_session() as session:
+            stmt = (
+                select(UserRep)
+                .where(
+                    UserRep.guild_id == ctx.guild.id,
+                    UserRep.user_id == user.id,
+                )
+                .limit(1)
+            )
+            result = await session.execute(stmt)
+            user_stats = result.scalar_one_or_none()
+
+            given_rep = 0
+            if user != ctx.author:
+                stmt = (
+                    select(func.count())
+                    .select_from(RepAddHistory)
+                    .where(
+                        RepAddHistory.guild_id == ctx.guild.id,
+                        RepAddHistory.user_id == ctx.author.id,
+                        RepAddHistory.target_id == user.id,
+                    )
+                )
+                given_rep = (await session.execute(stmt)).scalar() or 0
+
+            if not user_stats:
+                embed = discord.Embed(
+                    title=f"{self.bot.error_emoji} No Data",
+                    description=f"**{user.display_name}** has no recorded rep.",
+                    colour=discord.Colour.red(),
+                )
+                await ctx.reply(embed=embed)
+                return
+
+            embed = discord.Embed(
+                title="Rep Info",
+                description=f"{self.bot.info_emoji} You have given this user `{given_rep:,}` rep."
+                if user != ctx.author
+                else None,
+                colour=discord.Colour.light_grey(),
+            )
+
+            embed.add_field(name="Rep", value=f"{user_stats.rep:,}", inline=True)
+
+            embed.set_author(
+                name=f"@{user.name}",
+                icon_url=user.display_avatar.url,
+            )
+            embed.set_footer(
+                text=f"@{ctx.author.name}",
+                icon_url=ctx.author.display_avatar.url,
+            )
+            embed.set_thumbnail(
+                url=user.display_avatar.url,
+            )
+
+            await ctx.reply(embed=embed)
+
+    # Leaderboard command
+    @rep_group.command(name="leaderboard", aliases=["lb", "top"])
+    @commands.cooldown(1, 5)
+    async def rep_leaderboard(self, ctx: commands.Context["TitaniumBot"]):
+        if not ctx.guild:
+            return
+
+        await ctx.defer()
+
+        async with get_session() as session:
+            stmt = (
+                select(UserRep)
+                .where(UserRep.guild_id == ctx.guild.id, UserRep.rep != 0)
+                .order_by(UserRep.rep.desc())
+                .limit(1000)
+            )
+            result = await session.execute(stmt)
+            top_users = result.scalars().all()
+
+            if not top_users:
+                embed = discord.Embed(
+                    title=f"{self.bot.error_emoji} No Data",
+                    description="No users have recorded XP or levels yet.",
+                    colour=discord.Colour.red(),
+                )
+                await ctx.reply(embed=embed)
+                return
+
+            pages = [
+                discord.Embed(
+                    title="Rep Leaderboard",
+                    description="\n".join(
+                        [
+                            f"{x * 15 + i}. <@{entry.user_id}> - `{entry.rep:,}`"
+                            for x, entry in enumerate(chunk, start=1)
+                        ]
+                    ),
+                    colour=discord.Colour.light_grey(),
+                ).set_author(
+                    name=ctx.guild.name,
+                    icon_url=ctx.guild.icon.url if ctx.guild.icon else None,
+                )
+                for i, chunk in enumerate(discord.utils.as_chunks(top_users, 15), start=0)
+            ]
+
+            pages[0].set_footer(
+                text=f"Controlling: @{ctx.author.name}"
+                if len(pages) > 1
+                else f"@{ctx.author.name}",
+                icon_url=ctx.author.display_avatar.url,
+            )
+
+            view = RepReloadPageView(embeds=pages, timeout=240, title="Rep Leaderboard")
+            await ctx.reply(embed=pages[0], view=view)
+
+    @rep_group.command(name="add", aliases=["plus"])
+    async def add_rep(self, ctx: commands.Context["TitaniumBot"], user: discord.Member) -> None:
+        if not ctx.guild:
+            raise ValueError("Guild only command but no guild available")
+
+        await ctx.defer()
+
+        if user.id in self.bot.opt_out:
+            embed = discord.Embed(
+                title=f"{self.bot.error_emoji} Opted Out",
+                description="This user has opted out of optional data collection and cannot use rep features.",
+                colour=discord.Colour.red(),
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        async with get_session() as session:
+            session.add(
+                RepAddHistory(
+                    user_id=ctx.author.id,
+                    target_id=user.id,
+                    guild_id=ctx.guild.id,
+                )
+            )
+
+            stmt = insert(UserRep).values(
+                guild_id=ctx.guild.id,
+                user_id=user.id,
+                rep=1,
+            )
+            stmt = stmt.on_conflict_do_update(
+                index_elements=["guild_id", "user_id"],
+                set_={
+                    "rep": UserRep.rep + 1,
+                },
+            ).returning(UserRep)
+
+            rep = (await session.execute(stmt)).scalar_one()
+
+        await ctx.reply(
+            embed=discord.Embed(
+                title=f"{self.bot.success_emoji} Done",
+                description=f"**1 rep** given to {user.mention} (`{rep.rep}` rep total)",
+                colour=discord.Colour.green(),
+            ),
+        )
+
+    @rep_group.command(name="remove", aliases=["minus"])
+    @commands.cooldown(1, 3)
+    async def remove_rep(
+        self, ctx: commands.Context["TitaniumBot"], user: discord.Member, amount: int
+    ) -> None:
+        if not ctx.guild:
+            raise ValueError("Guild only command but no guild available")
+
+        await ctx.defer()
+
+        if user.id in self.bot.opt_out:
+            embed = discord.Embed(
+                title=f"{self.bot.error_emoji} Opted Out",
+                description="This user has opted out of optional data collection and cannot use rep features.",
+                colour=discord.Colour.red(),
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        async with get_session() as session:
+            stmt = (
+                select(RepAddHistory)
+                .where(
+                    RepAddHistory.guild_id == ctx.guild.id,
+                    RepAddHistory.user_id == ctx.author.id,
+                    RepAddHistory.target_id == user.id,
+                )
+                .limit(amount)
+            )
+            history = list((await session.execute(stmt)).scalars())
+
+            if not history:
+                await ctx.reply(
+                    embed=discord.Embed(
+                        title=f"{self.bot.error_emoji} Nothing to Remove",
+                        description="You haven't given this user any rep before, so there is nothing to remove.",
+                        colour=discord.Colour.red(),
+                    ),
+                )
+                return
+
+            if len(history) != amount:
+                await ctx.reply(
+                    embed=discord.Embed(
+                        title=f"{self.bot.error_emoji} Too Much Rep",
+                        description=f"You have only given this user `{len(history)}` rep, so you can't remove `{amount - len(history):,}` extra rep.",
+                        colour=discord.Colour.red(),
+                    ),
+                )
+                return
+
+            await session.delete(history[0])
+
+            stmt = insert(UserRep).values(
+                guild_id=ctx.guild.id,
+                user_id=user.id,
+                rep=0,
+            )
+            stmt = stmt.on_conflict_do_update(
+                index_elements=["guild_id", "user_id"],
+                set_={
+                    "rep": UserRep.rep - amount,
+                },
+            ).returning(UserRep)
+
+            rep = (await session.execute(stmt)).scalar_one()
+
+        await ctx.reply(
+            embed=discord.Embed(
+                title=f"{self.bot.success_emoji} Done",
+                description=f"**1 rep** removed from {user.mention} (`{rep.rep}` rep total)",
+                colour=discord.Colour.green(),
+            ),
+        )
+
+    @rep_group.command(name="set")
+    @commands.has_permissions(administrator=True)
+    @commands.cooldown(1, 3)
+    async def set_rep(
+        self, ctx: commands.Context["TitaniumBot"], user: discord.Member, amount: int
+    ) -> None:
+        if not ctx.guild:
+            raise ValueError("Guild only command but no guild available")
+
+        await ctx.defer()
+
+        if user.id in self.bot.opt_out:
+            embed = discord.Embed(
+                title=f"{self.bot.error_emoji} Opted Out",
+                description="This user has opted out of optional data collection and cannot use rep features.",
+                colour=discord.Colour.red(),
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        async with get_session() as session:
+            stmt = insert(UserRep).values(
+                guild_id=ctx.guild.id,
+                user_id=user.id,
+                rep=amount,
+            )
+            stmt = stmt.on_conflict_do_update(
+                index_elements=["guild_id", "user_id"],
+                set_={"rep": amount},
+            ).returning(UserRep)
+
+            await session.execute(stmt)
+
+        await ctx.reply(
+            embed=discord.Embed(
+                title=f"{self.bot.success_emoji} Done",
+                description=f"Set {user.mention}'s rep to `{amount:,}`.",
+                colour=discord.Colour.green(),
+            ),
+        )
+
 
 async def setup(bot: TitaniumBot) -> None:
-    return
-    # await bot.add_cog(RepTestCog(bot))
+    await bot.add_cog(RepTestCog(bot))
