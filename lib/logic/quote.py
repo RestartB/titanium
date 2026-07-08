44652c3ef6e31a218fec9db6c502aacd9a963ff1
RestartB <79792966+RestartB@users.noreply.github.com>
2026-07-08 21:41:19 +0100

    feat: full rewrite of quote logic
    view moved to separate file, quote generation moved to logic folder, new dataclass for quote data / config crreated

---
+++ lib/logic/quote.py
@@ -0,0 +1,137 @@
+import asyncio
+import html
+import os
+import re
+from io import BytesIO
+
+import discord
+import jinja2
+from PIL import Image
+from playwright.async_api import async_playwright
+
+from lib.classes.img_tools import ImageTools
+from lib.classes.quote_config import QuoteData
+from lib.helpers.shorten import shorten_preserve
+
+
+# Create quote image function
+async def create_quote_image(data: QuoteData) -> discord.File:
+    image_data = BytesIO()
+    content = data.content
+
+    content = html.escape(content)
+
+    # Multiline code blocks
+    content = re.sub(r"```(.*?)```", r"<code>\1</code>", content, flags=re.DOTALL)
+
+    raw_lines = content.splitlines()
+    processed_lines = []
+    has_spoilers = False
+
+    # Process markdown formatting
+    for line in raw_lines:
+        # 4chan Greentext
+        if line.startswith("&gt;"):
+            line = f"<span style='color: green;'>{line}</span>"
+
+        # Remove header characters
+        line = line.lstrip("### ").lstrip("## ").lstrip("# ")
+
+        # Bold
+        line = re.sub(r"\*\*(.*?)\*\*", r"<strong>\1</strong>", line)
+
+        # Underline
+        line = re.sub(r"__(.*?)__", r"<u>\1</u>", line)
+
+        # Strikethrough
+        line = re.sub(r"~~(.*?)~~", r"<s>\1</s>", line)
+
+        # Italics
+        line = re.sub(r"(?<!\*)\*([^*]+?)\*(?!\*)", r"<em>\1</em>", line)
+        line = re.sub(r"(?<!_)_([^_]+?)_(?!_)", r"<em>\1</em>", line)
+
+        # Code
+        line = re.sub(r"`([^`]+?)`", r"<code>\1</code>", line)
+        line = re.sub(r"```(.*?)```", r"<code>\1</code>", line)
+
+        # Check for spoilers
+        spoilers = re.findall(r"\|\|(.*?)\|\|", line)
+        if spoilers:
+            line = re.sub(r"\|\|(.*?)\|\|", r"\1", line)
+            has_spoilers = True
+
+        # Discord emojis
+        discord_emojis = re.findall(r"&lt;a?:\w+:\d+&gt;", line)
+
+        processed_lines.append(line)
+
+    content = "<br>".join(processed_lines)
+
+    # Replace Discord emojis with image tags
+    for emoji in discord_emojis:
+        emoji: str
+        emoji_id = emoji.split(":")[2].rstrip("&gt;")
+        content = content.replace(
+            emoji,
+            f"<img src='https://cdn.discordapp.com/emojis/{html.escape(emoji_id)}.png' height='44' alt='{emoji}' />",
+        )
+
+    # Render Jinja2 template
+    env = jinja2.Environment(
+        enable_async=True,
+        loader=jinja2.FileSystemLoader(os.path.join("lib", "templates")),
+        autoescape=True,
+    )
+    template = env.get_template("quote.jinja")
+    quote_html = await template.render_async(
+        content=content,
+        user=data.user,
+        nickname=data.nickname,
+        fade=data.fade,
+        light_mode=data.light_mode,
+        bw_mode=data.bw_mode,
+        custom_quote=data.custom_quote,
+        custom_quote_user=data.runner_user,
+        is_bot=data.user.bot,
+    )
+
+    async with async_playwright() as p:
+        # Launch browser
+        browser = await p.chromium.launch()
+        page = await browser.new_page(viewport={"width": 1200, "height": 600})
+
+        # Set HTML content
+        await page.set_content(quote_html)
+
+        # Wait for images
+        await page.wait_for_selector("body.ready")
+
+        # Take screenshot as bytes
+        screenshot = await page.screenshot(
+            type="png",
+            full_page=False,
+            clip={"x": 0, "y": 0, "width": 1200, "height": 600},
+        )
+
+        # Write to BytesIO
+        image_data.write(screenshot)
+        await browser.close()
+
+    if data.output_format != "PNG":
+        tools = ImageTools()
+        image_data = await asyncio.to_thread(
+            tools._save_sync,
+            img=Image.open(image_data),
+            output_format=data.output_format,
+            quality=95,
+        )
+
+    image_data.seek(0)
+    data.image_data = image_data
+
+    return discord.File(
+        image_data,
+        filename=f"titanium_quote.{data.output_format.value.lower()}",
+        spoiler=has_spoilers,
+        description=shorten_preserve(data.content, width=1024),
+    )
---
+++ lib/views/quote.py
@@ -0,0 +1,108 @@
+import dataclasses
+from typing import TYPE_CHECKING
+
+import discord
+from discord import ButtonStyle, Colour, Interaction
+from discord.ui import Button, View, button
+
+from lib.classes.quote_config import QuoteData
+from lib.enums.images import ImageFormats
+from lib.logic.quote import create_quote_image
+
+if TYPE_CHECKING:
+    from main import TitaniumBot
+
+
+class QuoteView(View):
+    def __init__(self, bot: TitaniumBot, data: QuoteData):
+        super().__init__(timeout=3600 * 24)
+        self.bot = bot
+        self.data = data
+
+        if not data.fade:
+            self.fade_mode.label = "Enable Fade"
+            self.fade_mode.emoji = "🕶️"
+            self.fade_mode.style = ButtonStyle.green
+
+        if data.bw_mode:
+            self.colour_mode.label = "Colour"
+            self.colour_mode.emoji = "🎨"
+
+        if data.light_mode:
+            self.theme_mode.label = "Dark Mode"
+            self.theme_mode.emoji = "🌙"
+
+    @button(label="Disable Fade", emoji="👓", style=ButtonStyle.red)
+    async def fade_mode(self, interaction: Interaction["TitaniumBot"], button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        if self.data.fade:
+            self.data.fade = False
+            self.fade_mode.label = "Enable Fade"
+            self.fade_mode.emoji = "🕶️"
+            self.fade_mode.style = ButtonStyle.green
+        else:
+            self.data.fade = True
+            self.fade_mode.label = "Disable Fade"
+            self.fade_mode.emoji = "👓"
+            self.fade_mode.style = ButtonStyle.red
+
+        file = await create_quote_image(self.data)
+        await interaction.edit_original_response(attachments=[file], view=self)
+
+    @button(label="Black and White", emoji="⚫", style=ButtonStyle.grey)
+    async def colour_mode(self, interaction: Interaction["TitaniumBot"], button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        if self.data.bw_mode:
+            self.data.bw_mode = False
+            self.colour_mode.label = "Black and White"
+            self.colour_mode.emoji = "⚫"
+        else:
+            self.data.bw_mode = True
+            self.colour_mode.label = "Colour"
+            self.colour_mode.emoji = "🎨"
+
+        file = await create_quote_image(self.data)
+        await interaction.edit_original_response(attachments=[file], view=self)
+
+    @button(label="Light Mode", emoji="☀️", style=ButtonStyle.grey)
+    async def theme_mode(self, interaction: Interaction["TitaniumBot"], button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        if self.data.light_mode:
+            self.data.light_mode = False
+            self.theme_mode.label = "Light Mode"
+            self.theme_mode.emoji = "☀️"
+        else:
+            self.data.light_mode = True
+            self.theme_mode.label = "Dark Mode"
+            self.theme_mode.emoji = "🌙"
+
+        file = await create_quote_image(self.data)
+        await interaction.edit_original_response(attachments=[file], view=self)
+
+    @button(label="PNG", emoji="🖼️", style=ButtonStyle.grey)
+    async def png_button(self, interaction: Interaction["TitaniumBot"], button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        copy_data = dataclasses.replace(self.data)
+        copy_data.output_format = ImageFormats.PNG
+
+        file = await create_quote_image(copy_data)
+        await interaction.followup.send(file=file, ephemeral=True)
+
+    @button(emoji="🗑️", style=ButtonStyle.red)
+    async def delete_button(self, interaction: Interaction["TitaniumBot"], button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        if self.data.user.id != interaction.user.id or not interaction.permissions.manage_messages:
+            embed = discord.Embed(
+                title=f"{interaction.client.error_emoji} Not Allowed",
+                description="You didn't send the original message. Only the original message author or users with Manage Message permissions can delete the quote.",
+                colour=Colour.red(),
+            )
+            await interaction.followup.send(embed=embed, ephemeral=True)
+            return
+
+        await interaction.delete_original_response()
