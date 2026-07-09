388c5ce6f6603108699dffa36b9eed4513e54a83
RestartB <79792966+RestartB@users.noreply.github.com>
2026-07-09 19:34:37 +0100

    fix: ignore bots in automod
    this blocks people from being able to cause infinite loops

--- cogs/moderation/automod.py
+++ cogs/moderation/automod.py
@@ -63,7 +63,7 @@ class AutomodMonitorCog(commands.Cog):
                 or not isinstance(message.author, discord.Member)
                 or not isinstance(message.channel, discord.abc.GuildChannel)
                 or not self.bot.user
-                or message.author.id == self.bot.user.id
+                or message.author.bot
                 or message.author.guild_permissions.administrator
             ):
                 self.logger.debug("Automod initial checks failed, skipping message")
