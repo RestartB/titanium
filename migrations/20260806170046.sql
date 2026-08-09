02fa8133d02c552e92f3d18849f96459a69c439e
RestartB <79792966+RestartB@users.noreply.github.com>
2026-08-09 19:06:17 +0100

    fix: default bouncer rule order to 0

--- migrations/20260806170046.sql
+++ migrations/20260806170046.sql
@@ -5,4 +5,4 @@ ALTER TABLE "bouncer_criteria" ADD COLUMN "match_all_words" boolean NOT NULL DEF
 -- Set null rule names to ""
 UPDATE "bouncer_rules" SET "rule_name" = '' WHERE "rule_name" IS NULL;
 -- Modify "bouncer_rules" table
-ALTER TABLE "bouncer_rules" ALTER COLUMN "rule_name" SET NOT NULL, ADD COLUMN "match_all_criteria" boolean NOT NULL DEFAULT true, ADD COLUMN "order" integer NOT NULL, ADD COLUMN "stop_if_triggered" boolean NOT NULL DEFAULT false;
+ALTER TABLE "bouncer_rules" ALTER COLUMN "rule_name" SET NOT NULL, ADD COLUMN "match_all_criteria" boolean NOT NULL DEFAULT true, ADD COLUMN "order" integer NOT NULL DEFAULT 0, ADD COLUMN "stop_if_triggered" boolean NOT NULL DEFAULT false;
